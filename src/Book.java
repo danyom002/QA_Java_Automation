4cdfd22bbbb991fa7c1f0f906d287305bc3a3979
danyom002 <akintoyedaniel321@gmail.com>
2023-11-30 02:31:19 -0500

    first commit

---
+++ src/Book.java
@@ -0,0 +1,39 @@
+public class Book {
+
+    private String title;
+    private String author;
+    private String publishedDate;
+    private String journal;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Book book = new Book();
+
+        book.setAuthor("Peme Chodron");
+        book.setTitle("Start where you are");
+        book.setPublishedDate("Sep, 13, 2023");
+
+    }
+}
